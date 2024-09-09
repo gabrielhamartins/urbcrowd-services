9093f67550667d3bcf8505836cf2fccf44fb6253
Gabriel Martins <gabriel.hamartins@gmail.com>
2024-09-09 20:33:24 -0300

    First commit.

---
+++ src/main/java/com/unicamp/urbcrowd/UrbcrowdApplication.java
@@ -0,0 +1,13 @@
+package com.unicamp.urbcrowd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbcrowdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbcrowdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicamp/urbcrowd/UrbcrowdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicamp.urbcrowd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbcrowdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
